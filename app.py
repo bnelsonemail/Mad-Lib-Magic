b22c95c0dc15935f5c354af492460d133e78a967
bnelson <bnelsonemail@icloud.com>
2024-08-18 12:10:38 -0400

    changed the name of app.py, reinstalled requirements applications, installed debugger toolbar, added styling in CSS, revised home.html, added app routes to app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -22,7 +22,12 @@ def form():
 
 @app.route('/stories')
 def stories():
-    return render_template('stories.html')
+    place = request.args['place']
+    noun = request.args["noun"]
+    verb = request.args['verb']
+    adjective = request.args['adjective']
+    plural_noun = request.args['plural_noun']
+    return render_template('stories.html', place = place, noun = noun, verb = verb, adjective = adjective, plural_noun = plural_noun)
 
 
 
